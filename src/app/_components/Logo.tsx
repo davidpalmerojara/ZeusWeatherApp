f116bc0545d122d9f80dbd0816423126a51cf066
David Palmero Jara <110841001+davidpalmerojara@users.noreply.github.com>
2025-05-27 19:25:54 +0200

    fix: specify subset for Baloo Tamma 2

--- src/app/_components/Logo.tsx
+++ src/app/_components/Logo.tsx
@@ -1,15 +1,18 @@
-import Image from "next/image";
-import { Baloo_Tamma_2 } from "next/font/google";
-import Link from "next/link";
+import Image from 'next/image';
+import { Baloo_Tamma_2 } from 'next/font/google';
+import Link from 'next/link';
 
-const baloo_Tamma_2 = Baloo_Tamma_2({ weight: "800" });
+// ✅ Agregamos el subset requerido
+const baloo_Tamma_2 = Baloo_Tamma_2({
+  weight: '800',
+  subsets: ['latin'], // <- requerido si preload está activo (por defecto lo está)
+});
 
 export default function Logo() {
   return (
     <Link href="/">
       <h1
-        className={`${baloo_Tamma_2.className} text-2xl font-bold tracking-tight`}
-      >
+        className={`${baloo_Tamma_2.className} text-2xl font-bold tracking-tight`}>
         ZE<span className="text-amber-600">Ú</span>S
       </h1>
     </Link>
